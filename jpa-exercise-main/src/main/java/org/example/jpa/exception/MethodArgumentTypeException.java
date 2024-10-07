3907a88dabd81112a0a48d298a326d9a4ecf815f
firemagic6 <yichengstudy@gmail.com>
2024-10-07 14:22:08 +0800

    Add files via upload

---
+++ jpa-exercise-main/src/main/java/org/example/jpa/exception/MethodArgumentTypeException.java
@@ -0,0 +1,8 @@
+package org.example.jpa.exception;
+
+public class MethodArgumentTypeException extends RuntimeException {
+
+    public MethodArgumentTypeException(String message) {
+        super(message);
+    }
+}
